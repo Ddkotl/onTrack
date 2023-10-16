07f799a846fe041e878d5108da9857e3514cf360
Ddkotl <no64129@gmail.com>
2023-10-16 23:30:55 +0300

    header/footer

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,12 @@
+<script setup>
+import TheLogo from './TheLogo.vue'
+import TheProgress from './TheProgress.vue'
+</script>
+<template>
+  <header
+    class="sticky top-0 z-20 flex items-center justify-between border-b border-violet-900 bg-white p-3"
+  >
+    <TheLogo />
+    <TheProgress />
+  </header>
+</template>
